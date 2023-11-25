009db2ddbdb177e5e0ecbb8c5a1cad9f975de2d1
Ximer <20438890+Ximer612@users.noreply.github.com>
2023-11-25 16:49:24 +0100

    Finished all windows 95 npcs

--- Assets/External Packages/LowPolyOfficeProps_FULL/Scripts/ClockHands.cs
+++ Assets/External Packages/LowPolyOfficeProps_FULL/Scripts/ClockHands.cs
@@ -8,23 +8,26 @@ public class ClockHands : MonoBehaviour {
     private const float
       hoursToDegrees = 360f / 12f,
       minutesToDegrees = 360f / 60f;
-     // secondsToDegrees = 360f / 60f;
 
-    public Transform hours, minutes;//, seconds;
+    public Transform hours, minutes;
 
-    // Use this for initialization
-    void Start () {
-		
-	}
+    private DateTime time;
+    [SerializeField] int hour, minute, seconds;
+
+    void Start () 
+    {
+        time = DateTime.Now;
+    }
 	
-	// Update is called once per frame
 	void Update () {
-      
-        // sets the angle of the hands based on the system time.
 
-        DateTime time = DateTime.Now;
-        hours.localRotation = Quaternion.Euler(0f, 0f, ((time.Hour ) * hoursToDegrees)+ ((time.Minute * minutesToDegrees) / 12.0f));
+        hour = time.Hour;
+        minute = time.Minute;
+        seconds = time.Second;
+
+        time.AddMilliseconds(Time.deltaTime);
+
+        hours.localRotation = Quaternion.Euler(0f, 0f, ((time.Hour) * hoursToDegrees) + ((time.Minute * minutesToDegrees) / 12.0f));
         minutes.localRotation = Quaternion.Euler(0f, 0f, (time.Minute * minutesToDegrees));
-        
     }
 }
